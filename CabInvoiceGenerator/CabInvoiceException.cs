7cac1869706126b84b3d26ba22eb6a1cefbc4fd8
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-12-12 13:20:11 +0530

    Added FIle

---
+++ CabInvoiceGenerator/CabInvoiceException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class CabInvoiceException : Exception
+    {
+        public enum ExceptionType
+        {
+            INVALID_RIDE_TYPE,
+            INVALID_DISTANCE,
+            INVALID_TIME,
+            INVALID_USER_ID,
+            NULL_RIDES
+        }
+        ExceptionType type;
+        public CabInvoiceException(ExceptionType type,string message):base(message)
+        {
+            this.type = type;
+        }
+    }
+}
---
+++ CabInvoiceGenerator/InvoiceGenertor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class InvoiceGenertor
+    {
+        RideType objRideType;
+        private RideRepository objRideRepository;
+        private readonly double MIN_COST_PER_KM;
+        private readonly int COST_PER_TIME;
+        private readonly double MIN_FARE;
+        public InvoiceGenertor(RideType objRideType)
+        {
+            this.objRideType = objRideType;
+            this.objRideRepository = objRideRepository;
+            try
+            {
+                if(objRideType.Equals(RideType.PREMIUM))
+                {
+                    this.MIN_COST_PER_KM = 15;
+                    this.COST_PER_TIME = 2;
+                    this.MIN_FARE = 20;
+                }
+                else if(objRideType.Equals(RideType.NORMAL))
+                {
+
+                }
+            }
+            catch(CabInvoiceException)
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_RIDE_TYPE,"Invalid Ride Type");
+            }
+        }
+    }
+}
